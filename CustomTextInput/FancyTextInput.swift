7b0e3f2e4ae830d5cdf3a8a98693647fa6244745
George Drag <gdrag@redfoundry.com>
2018-01-17 18:09:42 -0600

    basic properties and animations in place

--- CustomTextInput/FancyTextInput.swift
+++ CustomTextInput/FancyTextInput.swift
@@ -8,12 +8,25 @@
 
 import UIKit
 
+@IBDesignable
 class FancyTextInput: UIView, UITextFieldDelegate {
     
     @IBOutlet var contentView: UIView!
     @IBOutlet var textField: UITextField!
     @IBOutlet var lineView: UIView!
     @IBOutlet var lineViewWidth: NSLayoutConstraint!
+    @IBOutlet var placeHolderTextLabel: UILabel!
+    @IBOutlet var placeholderTextLeading: NSLayoutConstraint!
+    @IBOutlet var textFieldTopConstraint: NSLayoutConstraint!
+    @IBOutlet var labelBottomConstraint: NSLayoutConstraint!
+    
+    
+    @IBInspectable
+    var inputTextColor: UIColor = .black {
+        didSet {
+            updateView()
+        }
+    }
     
     @IBInspectable
     var placeholderText: String = "" {
@@ -29,26 +42,55 @@ class FancyTextInput: UIView, UITextFieldDelegate {
         }
     }
     
+    @IBInspectable
+    var underlineColor: UIColor = .gray {
+        didSet {
+            updateView()
+        }
+    }
+    
     fileprivate func updateView() {
-        textField.text = placeholderText
+        placeHolderTextLabel.text = placeholderText
         textField.textColor = placeholderTextColor
         textField.setLeftPaddingPoints(15)
+        placeholderTextLeading.constant = 15
+        lineView.layer.backgroundColor = underlineColor.cgColor
     }
     
     // MARK:- ---> UITextFieldDelegate
     func textFieldDidBeginEditing(_ textField: UITextField) {
         // became first responder
-        print("TextField did begin editing method called")
-        animateUnderline()
+        animateUnderline(focus: true)
     }
+    
 
-    func animateUnderline() {
-        self.lineViewWidth.constant = self.frame.width
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        animateUnderline(focus: false)
+        self.endEditing(true)
+        return true
+    }
+    
+    func animateUnderline(focus: Bool) {
+        self.lineViewWidth.constant = focus ? self.frame.width : 0
         UIView.animate(withDuration: 0.25) {
             self.layoutIfNeeded()
         }
     }
-
+    
+    func doResizeAnmimation() {
+        textField.textColor = inputTextColor
+        textFieldTopConstraint.constant = self.frame.height * 0.3
+        // add to bottom constraint of label
+        labelBottomConstraint.constant = -(self.frame.height * 0.6)
+        textField.setLeftPaddingPoints(10)
+        placeholderTextLeading.constant = -1
+        UIView.animate(withDuration: 0.3) {
+            self.placeHolderTextLabel.transform = CGAffineTransform(scaleX: 0.7, y: 0.7)
+            self.layoutIfNeeded()
+        }
+    }
+    
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -66,8 +108,14 @@ class FancyTextInput: UIView, UITextFieldDelegate {
         contentView.frame = self.bounds
         contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
         textField.delegate = self
+        textField.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: .editingChanged)
     }
     
+    @objc func textFieldDidChange(textField: UITextField) {
+        if textField.text?.count == 1 {
+            doResizeAnmimation()
+        }
+    }
 }
 
 extension UITextField {
